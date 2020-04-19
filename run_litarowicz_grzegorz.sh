b35b2e09605e43e677ad70d95caef108f0345071
BIGbadEL <greg090912@gmail.com>
2020-04-19 16:03:34 +0200

    solution

--- run_litarowicz_grzegorz.sh
+++ run_litarowicz_grzegorz.sh
@@ -10,24 +10,26 @@ fi
 
 #solution
 if [ "$command" = "solution" ]; then
-    echo cloning repo with solutions...
+    echo [solution] Pobieranie repo wraz z rozwiązaniami
     git clone https://github.com/Blinkuu/assembler-x86-and-x86-64.git
-    cat assembler-x86-and-x86-64/exercises/ex01/input.asm > exercises/ex01/is_triangle.asm
-    cp assembler-x86-and-x86-64/exercises/ex01/functions.asm exercises/ex01/
+    mkdir exercises/ex01-solution
+    cp exercises/ex01/* exercises/ex01-solution/
+    cat assembler-x86-and-x86-64/exercises/ex01/input.asm > exercises/ex01-solution/is_triangle.asm
+    cp assembler-x86-and-x86-64/exercises/ex01/functions.asm exercises/ex01-solution/
     cd exercises/ex01/
     ./run.sh is_triangle test
 fi
 
 if [ "$command" = "run" ]; then
-    cd exercises/ex01/
-    ./run.sh is_triangle test
+    echo [RUN]: zadanie znajduje się w exercises/ex01/
 fi
 
 if [ "$command" = "clean" ]; then
     if [[ ! -d "Grzegorz-Litarowicz---assembler-x86-and-x86-64" ]]
     then
+        echo [CLEAN]: znajdujesz się w folderze Grzegorz-Litarowicz---assembler-x86-and-x86-64
         cd .. 
     fi
+    echo [CLEAN]: usuwanie Grzegorz-Litarowicz---assembler-x86-and-x86-64
     rm -rf Grzegorz-Litarowicz---assembler-x86-and-x86-64
-    echo cloning repo with solutions...
 fi
