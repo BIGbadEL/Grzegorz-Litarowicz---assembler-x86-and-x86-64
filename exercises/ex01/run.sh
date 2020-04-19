3a24d35e04e0785e690db53e502d3f14a6b4e7f8
BIGbadEL <greg090912@gmail.com>
2020-04-19 17:26:02 +0200

    run

--- exercises/ex01/run.sh
+++ exercises/ex01/run.sh
@@ -2,8 +2,8 @@
 test=$2
 if [ "$test" = "test" ]; then
     nasm -f elf64 $1.asm && ld $1.o
-    ./a.out < ../test/good.txt
-    ./a.out < ../test/wrong.txt
+    ./a.out < ../good.txt
+    ./a.out < ../wrong.txt
 else
     nasm -f elf64 $1.asm && ld $1.o && ./a.out
 fi
